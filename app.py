de7f3ecda08f29e38a080a6aec29716c35dd3ddc
Jayveer722 <85777869+Jayveer722@users.noreply.github.com>
2022-12-03 21:38:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
-pipe = pickle.load(open('pipe.pkl','rb'))
 st.title('IPL Win Predictor')
 
 col1, col2 = st.beta_columns(2)
